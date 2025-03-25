1586689723391787234211cc4a7054d8464a77be
LindaTancini <lindatancini95@gmail.com>
2025-03-25 15:29:56 +0100

    feat: added bonus

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 console.log("Hello Boolean");
+// uso process.argv per prendere l'argomento che passo nel terminale
+const bonus = process.argv[2];
+// vedo l'array
+console.log(process.argv);
+// mostro l'argomento che passo scrivendo nel terminale
+console.log(bonus);
